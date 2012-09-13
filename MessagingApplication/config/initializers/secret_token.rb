f82f95353f9933488a0ab34e9defa8fe6710af26
Alex <alpanoob@gmail.com>
2012-09-13 21:31:31 +0200

    clean project added to the repo

---
+++ MessagingApplication/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MessagingApplication::Application.config.secret_token = 'e4d2327ced76f1f45d66c709ad234cee8204f854ee07dc70c0871eba0aefdf33907b9544015d919f971b057116f76c2f3f60c773c69e0d0f89192761ba05031e'
