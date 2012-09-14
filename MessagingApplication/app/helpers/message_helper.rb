f743656d2bee105c588429e1136819e8f5682d87
Alex <alpanoob@gmail.com>
2012-09-14 22:19:57 +0200

    asda

--- MessagingApplication/app/helpers/message_helper.rb
+++
@@ -1,8 +0,0 @@
-# encoding: utf-8
-module MessageHelper
-  def back_to_overview
-    content_tag(:p,
-    link_to("Zurück zur Nachrichtenübersicht", messages_path),
-    class: "subNavigation")
-  end
-end
--- MessagingApplication/app/models/message.rb
+++
@@ -1,5 +0,0 @@
-class Message < ActiveRecord::Base
-  validates :sender, :receiver, :subject, :content, presence: true
-  
-  attr_accessible :sender, :receiver, :subject, :content
-end
